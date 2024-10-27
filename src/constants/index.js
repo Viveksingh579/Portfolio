d6fccbb79db13802097168fc5c10dba3f3abc9ce
Viveksingh579 <vivek579singh@gmail.com>
2024-10-27 21:53:55 +0530

    first commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,36 +9,7 @@ export const HERO_CONTENT = `I am a passionate full stack developer with a stron
 
 export const ABOUT_TEXT = `I am an accomplished and versatile full stack developer with a strong passion for designing efficient and user-friendly web applications. Despite being relatively early in my professional journey, I have been actively enhancing my skills in technologies such as React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB through diverse projects and learning experiences. My fascination with understanding how things work has evolved into a steadfast commitment to continuously learn and tackle new challenges. I excel in collaborative environments and derive satisfaction from resolving intricate issues to deliver high-quality solutions. Beyond coding, I actively engage in staying abreast of the latest technologies and contributing to open-source projects.`;
 
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
+
 
 export const PROJECTS = [
   {
@@ -54,7 +25,7 @@ export const PROJECTS = [
     image: project2,
     description:
       "A MERN stack application for creating and taking interactive quizzes, featuring user authentication, role-based access, and dynamic quiz management..",
-    technologies: ["HTML", "CSS", "React-js", "NodeJs","MongoDb"],
+    technologies: ["HTML", "CSS", "React-js", "NodeJs","Express JS","MongoDb"],
     githubLink: 'https://github.com/Viveksingh579/Quiz-WEB',
 
   },
@@ -64,7 +35,7 @@ export const PROJECTS = [
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
-    githubLink: 'https://github.com/username/project1',
+    githubLink: 'https://vivekportfolio-24.netlify.app/',
   }
   
 ];
